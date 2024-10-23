ccd7fbb3d32ca137d99b4bcd8e20cfc947d65df8
martin3698753 <martinek8425@gmail.com>
2024-10-23 22:20:26 +0200

    test

--- graph.py
+++ graph.py
@@ -4,15 +4,15 @@ import numpy as np
 
 data = pd.read_csv('data.csv')
 t = np.arange(data.shape[0])
-#data=(data-data.min())/(data.max()-data.min())
+data=(data-data.min())/(data.max()-data.min())
 axes = list(data)
-#for a in axes:
-    #plt.plot(t, data[a], label=a)
+for a in axes:
+    plt.plot(t, data[a], label=a)
 
-plt.plot(t, data['pm.batteryLevel'], label='level_bat')
-plt.plot(t, data['pm.vbat'], label='voltage_bat')
+#plt.plot(t, data['pm.batteryLevel'], label='level_bat')
+#plt.plot(t, data['pm.vbat'], label='voltage_bat')
 
 plt.legend()
-plt.savefig('bat.png')
+plt.savefig('norm.png')
 
 
--- takeoff.py
+++ takeoff.py
@@ -1,12 +1,10 @@
 import logging
 import sys
 import time
-import numpy as np
 from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
@@ -14,34 +12,15 @@ from cflib.utils import uri_helper
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 
 DEFAULT_HEIGHT = 0.5
-BOX_LIMIT = 0.3
-title = False
 
 deck_attached_event = Event()
 
 logging.basicConfig(level=logging.ERROR)
 
-def move_circ(scf):
+def take_off_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        while(1):
-            mc.start_circle_left(radius_m = 0.3)
-
-
-def log_pos_callback(timestamp, data, logconf):
-    print(data)
-    global title
-    names = np.array(list(data.items()))
-    names = names[:,0]
-    f = open("data.csv", "a")
-    for n in names:
-        if not title:
-            for name in names:
-                f.write(name+',')
-            f.write('\n')
-            title = True
-        f.write(str(data[n]) + ',')
-    f.write('\n')
-    f.close()
+        time.sleep(3)
+        mc.stop()
 
 
 def param_deck_flow(_, value_str):
@@ -53,7 +32,6 @@ def param_deck_flow(_, value_str):
     else:
         print('Deck is NOT attached!')
 
-
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
@@ -63,25 +41,9 @@ if __name__ == '__main__':
                                          cb=param_deck_flow)
         time.sleep(1)
 
-        logconf = LogConfig(name='Position', period_in_ms=10)
-        logconf.add_variable('stateEstimate.x', 'float')
-        logconf.add_variable('stateEstimate.y', 'float')
-        logconf.add_variable('stateEstimate.z', 'float')
-
-        logconf.add_variable('pm.vbatMV', 'float')
-        logconf.add_variable('pm.batteryLevel', 'float')
-
-        scf.cf.log.add_config(logconf)
-        logconf.data_received_cb.add_callback(log_pos_callback)
-
         if not deck_attached_event.wait(timeout=5):
             print('No flow deck detected!')
             sys.exit(1)
 
-        logconf.start()
-        move_circ(scf)
-        logconf.stop()
-
-
-
+        take_off_simple(scf)
 
